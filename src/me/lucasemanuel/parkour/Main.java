201f669f2268ab551f77632c87dac8541dd1ec07
Lucas Arnstrom <LucasArnstrom@gmail.com>
2012-08-18 17:57:05 +0200

    initial commit
    Signed-off-by: Lucas Arnstrom <LucasArnstrom@gmail.com>

---
+++ src/me/lucasemanuel/parkour/Main.java
@@ -0,0 +1,25 @@
+/**
+ *  Name: Main.java
+ *  Date: 17:52:00 - 18 aug 2012
+ * 
+ *  Author: LucasEmanuel @ bukkit forums
+ *  
+ *  
+ *  Description:
+ *  
+ *  
+ *  
+ * 
+ * 
+ */
+
+package me.lucasemanuel.parkour;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+	
+	public void onEnable() {
+		
+	}
+}
---
+++ src/me/lucasemanuel/parkour/SignManager.java
@@ -0,0 +1,20 @@
+/**
+ *  Name: SignManager.java
+ *  Date: 17:55:06 - 18 aug 2012
+ * 
+ *  Author: LucasEmanuel @ bukkit forums
+ *  
+ *  
+ *  Description:
+ *  
+ *  
+ *  
+ * 
+ * 
+ */
+
+package me.lucasemanuel.parkour;
+
+public class SignManager {
+
+}
